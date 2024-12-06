c9f4a5217d0b28bc505392608e542b16e51d6542
AzureAD\ReemMarji <reem.marji@gotocme.com>
2024-12-06 10:47:37 +0200

    fixing edge cases

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ st.title("Weather Forecast for the Next Days")
 
 place = st.text_input("Place: ")
 
+
 days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the number of forecasted days")
 
 option = st.selectbox("Select data to view",
@@ -14,24 +15,25 @@ option = st.selectbox("Select data to view",
 st.subheader(f"{option} for the next {days} days in {place}")
 
 # get data
+try:
+    if place:
 
-if place:
+        filtered_data = get_data(place, days)
 
-    filtered_data = get_data(place, days)
+        if option=='Temperature':
+            temperatures = [dict["main"]["temp"]/10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={'x': "Dates", 'y':"Temperature (c)"})
+            st.plotly_chart(figure)
 
-    if option=='Temperature':
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        figure = px.line(x=dates, y=temperatures, labels={'x': "Dates", 'y':"Temperature (c)"})
-        st.plotly_chart(figure)
+        if option=='Sky':
+            columns = st.columns(8)
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
 
-    if option=='Sky':
-        columns = st.columns(8)
-        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
-
-        for index, condition in enumerate(sky_conditions):
-            condition = condition.lower()
-            with columns[index % 8]: 
-                st.image(f"images/{condition}.png", width=115)
-        
+            for index, condition in enumerate(sky_conditions):
+                condition = condition.lower()
+                with columns[index % 8]: 
+                    st.image(f"images/{condition}.png", width=115)
+except KeyError:
+    st.text("Please enter a valid place!")       
 
